a1a981c7e299325d5bc3b2ded94803dfc9df50fc
StardustDL <stardustdl@163.com>
2022-09-22 08:32:32 +0800

    fix demo

--- demo/generator.py
+++ demo/generator.py
@@ -3,7 +3,7 @@ from generator_oj_problem.generators import data
 
 # Write your generator code
 from random import randint
-x, y = randint(), randint()
+x, y = randint(1, 1000), randint(1, 1000)
 
 # Write to input data, just like `print(x, y, file=input)`
 data.In(x, y)
